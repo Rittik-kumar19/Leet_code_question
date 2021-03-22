ecfd2b1cf0aa658fbea6cf8b9d91aba1058f0254
Rittik kumar <76815180+Rittik-kumar19@users.noreply.github.com>
2021-03-22 12:31:18 +0530

    Time: 20 ms (99.79%), Space: 27.5 MB (72.60%) - LeetHub

--- binary-search/binary-search.cpp
+++ binary-search/binary-search.cpp
@@ -1,20 +1,31 @@
 class Solution {
 public:
-    int search(vector<int>& nums, int target) {
-        int st=0,end=nums.size()-1;
-        // int mid=st+(end-st)/2;
-        while(st<=end){
-        int mid=st+(end-st)/2;
-        if(nums[mid]>target){
-            end=mid-1;
+    // int binsearch(vector<int>&nums,int target,int st,int end);
+    int search(vector<int>&nums, int target) {
+        // int st=0;
+        // int end=nums.size()-1;
+        // int ans=binsearch(nums,target,st,end);
+        // return ans;
+        auto lx=lower_bound(nums.begin(),nums.end(),target);
+        int xx=lx-nums.begin();
+        if(nums[xx]!=target){
+            return -1;
         }
-        else if(nums[mid]<target){
-            st=mid+1;
-        }
-        else if(nums[mid]==target){
-            return mid;
-        }
-        }
-        return -1;
+        return xx;
     }
+    // int binsearch(vector<int>&nums,int target,int st,int end){
+    //     if(st<=end){
+    //         int mid=st+(end-st)/2;
+    //     if(nums[mid]==target){
+    //         return mid;
+    //     }
+    //     else if(nums[mid]>target){
+    //         return binsearch(nums,target,st,mid-1);
+    //     }
+    //     else if(nums[mid]<target){
+    //         return binsearch(nums,target,mid+1,end);
+    //     }
+    //     }
+    //     return -1;
+    // }
 };
