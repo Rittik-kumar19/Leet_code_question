cceda814cc3c8aae4c3211799f1d1a4b74ac2722
Rittik kumar <76815180+Rittik-kumar19@users.noreply.github.com>
2021-04-23 14:25:10 +0530

    Time: 0 ms (100.00%), Space: 9.3 MB (33.26%) - LeetHub

--- longest-common-prefix/longest-common-prefix.cpp
+++ longest-common-prefix/longest-common-prefix.cpp
@@ -10,9 +10,17 @@ public:
         string a=strs[0];
         string b=strs[n-1];
         for(int i=0;i<a.size();i++){
-            if(a[i]==b[i]){
-                str+=a[i];
-            }else{
+            // if(a[i]==b[i]){
+            //     str+=a[i];
+            // }else{
+            //     break;
+            // }
+            int j=1;
+            if(a.substr(i,j)==b.substr(i,j)){
+                str=str+a.substr(i,j);
+                j++;
+            }
+            else{
                 break;
             }
         }
