7f5312940044d327e71053086b0d0708f2276ccc
Rittik kumar <76815180+Rittik-kumar19@users.noreply.github.com>
2021-04-25 16:05:27 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (48.94%) - LeetHub

---
+++ climbing-stairs/climbing-stairs.cpp
@@ -0,0 +1,22 @@
+int t[46]={0};
+class Solution {
+public:
+    int climbStairs(int n) {
+        
+       if(n==1) {
+           t[n]=1;
+           return t[n];
+       }
+        if(n==2){
+            t[n]=2;
+            return t[n];
+        }
+        if(t[n]!=0){
+            return t[n];
+        }
+        t[n]=climbStairs(n-1)+climbStairs(n-2);
+        return t[n];
+        
+        
+    }
+};
