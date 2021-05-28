95280d73a672bf555e22738f729293cc5627d3fb
Rittik kumar <76815180+Rittik-kumar19@users.noreply.github.com>
2021-05-28 07:55:58 +0530

    Time: 4 ms (68.96%), Space: 11.2 MB (43.35%) - LeetHub

---
+++ best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& arr) {
+        int n=arr.size();
+        int obsp=0-arr[0];
+        int ossp=0;
+        int ocsp=0;
+        for(int i=1;i<n;i++){
+            int nbsp=0;
+            int nssp=0;
+            int ncsp=0;
+            if(ocsp-arr[i]>obsp){
+                nbsp=ocsp-arr[i];
+            }
+            else{
+                nbsp=obsp;
+            }
+            if(obsp+arr[i]>ossp){
+                nssp=obsp+arr[i];
+            }
+            else{
+                nssp=ossp;
+            }
+            if(ossp>ocsp){
+                ncsp=ossp;
+            }
+            else{
+                ncsp=ocsp;
+            }
+            obsp=nbsp;
+            ossp=nssp;
+            ocsp=ncsp;
+        }
+        return ossp;
+    }
+};
