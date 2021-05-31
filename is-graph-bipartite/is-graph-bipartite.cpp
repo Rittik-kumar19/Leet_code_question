7cea20f679848ae33e60b5499125b9586f7fa8e8
Rittik kumar <76815180+Rittik-kumar19@users.noreply.github.com>
2021-05-31 14:21:08 +0530

    Time: 16 ms (98.80%), Space: 13.6 MB (46.18%) - LeetHub

--- is-graph-bipartite/is-graph-bipartite.cpp
+++ is-graph-bipartite/is-graph-bipartite.cpp
@@ -1,63 +1,35 @@
 class Solution {
 public:
-     bool DFS(vector<vector<int>>& graph, vector<int>& visited, int node, int color){
-        
-        for(int elem : graph[node]){
-            if(visited[elem]==-1){
-                visited[elem]=color;
-                bool isValid=DFS(graph, visited, elem, (visited[elem]+1)%2);
-                if(isValid==false)
-                    return false;
-            }
-            else if(visited[elem]!=color)
-                return false;
-        }
-        return true;
-    }
+
     bool isBipartite(vector<vector<int>>& graph) {
-        int n=graph.size();
-        if(n==0) return true;
-        
-        vector<int> visited(n, -1);
-        
-        for(int i=0; i<n; i++){
-            if(visited[i]==-1){
-                visited[i]=0;
-                bool isValid=DFS(graph, visited, i, (visited[i]+1)%2);
-                if(isValid==false)
+        int V=graph.size();
+        vector<int>vis(V,-1);
+        for(int i=0;i<V;i++){
+            if(vis[i]==-1){
+                if(!bfs(i,graph,V,vis)){
                     return false;
+                }
             }
         }
         return true;
     }
-    // bool isBipartite(vector<vector<int>>& graph) {
-    //     int V=graph.size();
-    //     vector<int>vis(V,-1);
-    //     for(int i=0;i<V;i++){
-    //         if(vis[i]==-1){
-    //             if(!Dfs(i,graph,V,vis)){
-    //                 return false;
-    //             }
-    //         }
-    //     }
-    //     return true;
-    // }
-    // bool Dfs(int src,vector<vector<int>>&graph,int V,vector<int>&vis){
-    //     if(vis[src]!=-1){
-    //         vis[src]=1;
-    //     }
-    //     for(auto x:graph[src]){
-    //         if(vis[x]==-1){
-    //             vis[x]=1-vis[src];
-    //             bool chh=Dfs(x,graph,V,vis);
-    //             if(!chh){
-    //                 return false;
-    //             }
-    //         }
-    //         else if(vis[x]!=vis[src]){
-    //             return false;
-    //         }
-    //     }
-    //     return true;
-    // }
+    bool bfs(int src,vector<vector<int>>&graph,int V,vector<int>&vis){
+        queue<int>q;
+        q.push(src);
+        vis[src]=1;
+        while(!q.empty()){
+            int node=q.front();
+            q.pop();
+            for(auto x:graph[node]){
+                if(vis[x]==-1){
+                    vis[x]=1-vis[node];
+                    q.push(x);
+                }
+                else if(vis[x]==vis[node]){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
 };
