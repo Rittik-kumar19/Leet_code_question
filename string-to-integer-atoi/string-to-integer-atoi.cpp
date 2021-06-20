e8139b53aab8cd5e6f4b7961e74deb8edc6b6254
Rittik kumar <76815180+Rittik-kumar19@users.noreply.github.com>
2021-06-20 23:21:17 +0530

    Time: 0 ms (100.00%), Space: 7 MB (74.34%) - LeetHub

--- string-to-integer-atoi/string-to-integer-atoi.cpp
+++ string-to-integer-atoi/string-to-integer-atoi.cpp
@@ -1,9 +1,32 @@
 class Solution {
 public:
-    int myAtoi(string s) {
-        stringstream data(s);
-        int ans = 0;
-        data>>ans;
-        return ans;
+   int myAtoi(string str) {
+        long long int res = 0;
+        string trim = str;
+        bool sign = false;
+        
+        while(trim[0] == ' ')
+            trim.erase(trim.begin());
+        
+        if(trim[0] == '-' || trim[0] == '+')
+        {
+            if(trim[0] == '-')
+                sign = true;
+            trim.erase(trim.begin());
+        }
+        
+        for(char ch : trim)
+        {
+            if(!(ch >= '0' && ch <= '9'))
+                break;
+            
+            res = res*10 + (ch-'0');
+            if(res > INT_MAX)
+                return (sign ? INT_MIN : INT_MAX);
+        }
+        
+        if(sign)
+            res = -1*res;
+        return res;
     }
 };
