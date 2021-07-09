816acf85427f3b4b80950797a4d67dd1776377b4
Rittik kumar <76815180+Rittik-kumar19@users.noreply.github.com>
2021-07-09 21:51:31 +0530

    Time: 12 ms (23.40%), Space: 13.5 MB (84.59%) - LeetHub

--- find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
+++ find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,81 +1,26 @@
 class Solution {
 public:
     vector<int> searchRange(vector<int>& nums, int target) {
-        // vector<int>ans;
-        // if(nums.size()==0){
-        //     ans.push_back(-1);
-        //     ans.push_back(-1);
-        //     return ans;
-        // }
-        // else if(nums.size()!=0){
-        // auto xx=equal_range(nums.begin(),nums.end(),target);
-        // int lw=xx.first-nums.begin();
-        // int up=xx.second-nums.begin();
-        // if(nums[lw]!=target || nums[up-1]!=target){
-        //     ans.push_back(-1);
-        //     ans.push_back(-1);
-        //     return ans;
-        // }
-        // ans.push_back(lw);
-        // ans.push_back(up-1);
-        // }
-        // return ans;
-        
-        vector<int>ans;
-        if(nums.size()==0){
-            ans.push_back(-1);
-            ans.push_back(-1);
-            // return ans;
+        int n = nums.size();
+        vector<int>v(2);
+        auto it1 = lower_bound(nums.begin(),nums.end(),target)-nums.begin();
+        auto it2 = upper_bound(nums.begin(),nums.end(),target)-nums.begin();
+        int x,y;
+        if(it1<n && nums[it1]==target){
+            x=it1;
+            v[0]=x;
         }
-        else if(nums.size()!=0){
-            int res1=0;
-        int res2=0;
-        int st=0,end=nums.size()-1;
-        int mid1=0;
-        while(st<=end){
-            mid1=st+(end-st)/2;
-            if(nums[mid1]<target){
-                st=mid1+1;
-            }
-            else if(nums[mid1]>target){
-                end=mid1-1;
-            }
-            else if(nums[mid1]==target){
-                res1=mid1;
-                end=mid1-1;
-            }
-        }
-        // if(res1==0){
-        //     res1=-1;
-        // }
-        int mid2=0;int sts=0;int ends=nums.size()-1;
-        while(sts<=ends){
-            mid2=sts+(ends-sts)/2;
-            if(nums[mid2]<target){
-                sts=mid2+1;
-            }
-            else if(nums[mid2]>target){
-                ends=mid2-1;
-            }
-            else if(nums[mid2]==target){
-                res2=mid2;
-                sts=mid2+1;
-            }
-        }
-        // if(res2==0){
-        //     res2=-1;
-        // }
-        if(nums[res1]!=target ||nums[res2]!=target){
-            ans.push_back(-1);
-            ans.push_back(-1);
-            // return ans;
+        it2--;
+        if(it2>=0 && nums[it2]==target){
+            y=it2;
+            v[1]=y;
+            return v;
         }
         else{
-            ans.push_back(res1);
-            ans.push_back(res2);
+            vector<int>vv(2,-1);
+            return vv;
         }
-        // return ans;
-        }
-        return ans;
+        
+        
     }
 };
