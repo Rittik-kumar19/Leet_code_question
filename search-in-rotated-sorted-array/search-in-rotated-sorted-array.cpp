d33ae946ac85cd7e9acfbc26ce24c096ed69391a
Rittik kumar <76815180+Rittik-kumar19@users.noreply.github.com>
2021-07-09 22:02:50 +0530

    Time: 0 ms (100.00%), Space: 11.2 MB (27.48%) - LeetHub

--- search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -1,29 +1,27 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        int st=0,end=nums.size()-1;
-        int mid=0;
-        while(st<=end){
-            mid=st+(end-st)/2;
-            if(nums[mid]==target){
-                return mid;
-            }
-            else if(nums[st]<=nums[mid]){//left sorted
-                if(target>=nums[st] && target<=nums[mid]){
-                    end=mid-1;
-                }
-                else{
-                    st=mid+1;
-                }
-            }
-            else {
-                if(target>=nums[mid] && target<=nums[end]) {
-                    st=mid+1;
-                }
-                else{
-                    end=mid-1;
-                }
-            }
+        int n = nums.size();
+        int l = 0,h = n-1;
+        // First find the index of lowest element
+        while(l<h){
+            int m = l + (h-l)/2;
+            if(nums[m]>nums[h]) l = m+1;
+            else h = m; 
+        }
+        int r = l; // store it in r
+        
+        if(target==nums[r]) return r;
+        else if(r==0) l=0,h=n-1;
+        else if(target>=nums[0]) l=0,h = r;
+        else if(target<nums[0]) l = r,h=n-1;
+        
+        //Simple Binary Search
+        while(l<=h){
+            int m = l + (h-l)/2;
+            if(nums[m]==target) return m;
+            if(nums[m]>target) h = m -1;
+            else l = m+1;
         }
         return -1;
     }
