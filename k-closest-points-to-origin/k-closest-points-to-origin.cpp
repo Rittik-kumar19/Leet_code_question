2fc0655ce6559e43b9266e7d51152dfbb5cf60a4
Rittik kumar <76815180+Rittik-kumar19@users.noreply.github.com>
2021-07-18 21:43:12 +0530

    Time: 156 ms (80.70%), Space: 54 MB (69.51%) - LeetHub

--- k-closest-points-to-origin/k-closest-points-to-origin.cpp
+++ k-closest-points-to-origin/k-closest-points-to-origin.cpp
@@ -1,28 +1,28 @@
 class Solution {
 public:
     vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
-        vector< vector<int> >vnew;
-        // vector<int>vv;
-        // map<pair<int,int>,int>mp;
-        priority_queue<pair<int,pair<int,int> > >maxh;
-        for(int i=0;i<points.size();i++){
-            maxh.push({points[i][0]*points[i][0]+points[i][1]*points[i][1],{points[i][0],points[i][1]}});
-            if(maxh.size()>k){
-                maxh.pop();
+        vector<vector<int>> ans;
+        if(points.size()==0)return ans;
+        priority_queue<pair<int,pair<int,int>>> pq;
+        for(int i=0;i<points.size();i++)
+        {
+            int dist=points[i][0]*points[i][0]+points[i][1]*points[i][1];
+            if(pq.size()<k)
+            pq.push({dist,{points[i][0],points[i][1]}});
+            else
+            {
+                if(dist<pq.top().first)
+                {
+                    pq.pop();
+                    pq.push({dist,{points[i][0],points[i][1]}});
+                }
             }
         }
-        while(k>0){
-            vector<int>v;
-            // v.push_back(maxh.top().second.first);
-            // v.push_back(maxh.top().second.second);
-            // vnew.push_back(v);
-            pair<int,int>p=maxh.top().second;
-            v.push_back(p.first);
-            v.push_back(p.second);
-            vnew.push_back(v);
-            maxh.pop();
-            k--;
+        while(!pq.empty())
+        {
+            ans.push_back({pq.top().second.first,pq.top().second.second});
+            pq.pop();
         }
-        return vnew;
+        return ans;
     }
 };
