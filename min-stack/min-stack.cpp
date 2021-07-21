8a131402177f63e7c731879fd9d23a4248013e20
Rittik kumar <76815180+Rittik-kumar19@users.noreply.github.com>
2021-07-22 01:12:14 +0530

    Time: 24 ms (53.70%), Space: 16.4 MB (31.70%) - LeetHub

--- min-stack/min-stack.cpp
+++ min-stack/min-stack.cpp
@@ -1,65 +1,52 @@
 class MinStack {
-    private:
-    stack<int> s1;    
-    stack<int> s2;   //for containing min value at top 
 public:
-    void push(int x) {
-	    s1.push(x);
-	    if(s2.empty() || x<=s2.top()) s2.push(x);	    
+    stack<int>s;
+    stack<int>ss;
+    /** initialize your data structure here. */
+    MinStack() {
     }
+    
+    void push(int val) {
+        s.push(val);
+        if(ss.size()==0 || val<=ss.top()){
+            ss.push(val);
+        }
+        return;
+    }
+    
     void pop() {
-	    if(s1.top()==s2.top()) s2.pop();
-	    s1.pop();
+        int x;
+        if(s.size()>0){
+            x=s.top();
+            s.pop();
+        }
+        if(ss.size()>0 and x==ss.top()){
+            ss.pop();
+        }
+        return;
     }
+    
     int top() {
-	    return s1.top();
+        if(s.size()>0){
+            return s.top();
+        }
+        return -1;
     }
+    
     int getMin() {
-	    return s2.top();
+        if(ss.size()>0){
+            return ss.top();
+        }
+        else{
+            return -1;
+        }
     }
-// public:
-//     /** initialize your data structure here. */
-//     stack<int>s;
-//     stack<int>ss;
-    
-//     void push(int x) {
-//         s.push(x);
-//         if(ss.size()>=0 && ss.top()>=x){
-//             ss.push(x);
-//         }
-//     }
-    
-//     void pop() {
-//        if(s.size()==0){
-//            return;
-//        }
-//         int x=s.top();
-//         s.pop();
-//         if(x==ss.top()){
-//             ss.pop();
-//             // cout<<x<<endl;
-//         }
-//         // cout<<x<<endl;
-//         return;
-        
-//     }
-    
-//     int top() {
-//         return s.top();
-//     }
-    
-//     int getMin() {
-//         if(ss.size()==0){
-//             return -1;
-//         }
-//         return ss.top();
-//     }
 };
 
 /**
  * Your MinStack object will be instantiated and called as such:
  * MinStack* obj = new MinStack();
- * obj->push(x);
+ * obj->push(val);
  * obj->pop();
  * int param_3 = obj->top();
  * int param_4 = obj->getMin();
