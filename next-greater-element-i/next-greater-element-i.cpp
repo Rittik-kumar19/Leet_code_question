ea8566df9847518ec5315e55f92fc78b89487ae6
Rittik kumar <76815180+Rittik-kumar19@users.noreply.github.com>
2021-07-22 14:20:18 +0530

    Time: 0 ms (100.00%), Space: 9.1 MB (5.49%) - LeetHub

--- next-greater-element-i/next-greater-element-i.cpp
+++ next-greater-element-i/next-greater-element-i.cpp
@@ -1,35 +1,80 @@
+// class Solution {
+// public:
+//     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+//        map<int,int>mp;
+//        for(int i=0;i<nums2.size();i++){
+//            mp[nums2[i]]=i;
+//        }
+//        vector<int>v;
+//        stack<int>st;
+//        for(int i=nums2.size()-1;i>=0;i--){
+//            if(st.size()==0){
+//                v.push_back(-1);
+//                // st.push(nums2[i]);
+//            }
+//            else if(st.size()>0 and st.top()>nums2[i]){
+//                v.push_back(st.top());
+//                // st.push(nums2[i]);
+//            }
+//            else if(st.size()>0 and st.top()<=nums2[i]){
+//                while(st.size()>0 and st.top()<=nums2[i]){
+//                    st.pop();
+//                }
+//            }
+//            if(st.size()==0){
+//                v.push_back(-1);
+//                // st.push(nums2[i]);
+//            }
+//            else{
+//                v.push_back(st.top());
+//                // st.push(nums2[i]);
+//            }
+//            st.push(nums2[i]);
+//        }
+//        reverse(v.begin(),v.end());
+//        vector<int>ans;
+//        for(int i=0;i<nums1.size();i++){
+//            int x=mp[nums1[i]];
+//            ans.push_back(v[x]);
+//        }
+//        return v;
+//     }
+// };
 class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-          map<int,int>m1; 
-    stack<int>st;
-     int n=nums2.size();
-    
-  for(int i=n-1;i>=0;i--){
-      
-      while(!st.empty() && st.top()<nums2[i]){
-          st.pop();
-      }
-
-      if(st.empty()){
-          m1[nums2[i]]=-1;
-      }
-      
-      else{
-          m1[nums2[i]]=st.top();
-      }
-      
-      st.push(nums2[i]);
-
-      
-  }
-    vector<int>ans;
-    
-    for(auto i:nums1){
-        ans.push_back(m1[i]);
-    }
-    
-    
-    return ans;
+        stack <int>s;
+        unordered_map<int,int>mp;
+        vector<int>v;
+        for(int i=0;i<nums2.size();i++){
+            mp[nums2[i]]=i;
+        }
+        for(int i=nums2.size()-1;i>=0;i--){
+            if(s.empty())
+                v.push_back(-1);
+            else if(!s.empty()&&nums2[i]<s.top()){
+                // mp[nums2[i]]=s.top();
+                v.push_back(s.top());
+            }
+            else if (!s.empty()&&nums2[i]>=s.top()){
+                while(!s.empty()&&nums2[i]>=s.top())
+                    s.pop();
+            if(s.empty())
+                // mp[nums2[i]]=-1;
+                v.push_back(-1);
+            else 
+                // mp[nums2[i]]=s.top();
+                v.push_back(s.top());
+                
+            }
+            s.push(nums2[i]);
+        }
+        reverse(v.begin(),v.end());
+        vector<int>ans;
+        for(int i=0;i<nums1.size();i++){
+            int x =mp[nums1[i]];
+            ans.push_back(v[x]);
+        }
+        return ans;
     }
 };
