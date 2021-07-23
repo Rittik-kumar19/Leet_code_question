b6d52b8c20e1e1e100546ee132d73ff7d4bc9ce7
Rittik kumar <76815180+Rittik-kumar19@users.noreply.github.com>
2021-07-24 03:56:28 +0530

    Time: 72 ms (12.05%), Space: 10.9 MB (13.36%) - LeetHub

--- first-unique-character-in-a-string/first-unique-character-in-a-string.cpp
+++ first-unique-character-in-a-string/first-unique-character-in-a-string.cpp
@@ -1,16 +1,29 @@
 class Solution {
 public:
     int firstUniqChar(string s) {
+        map<char,int>mp1;
+        map<char,int>mp2;
         int n=s.size();
-        map<char,int>mp;
         for(int i=0;i<n;i++){
-            mp[s[i]]++;
+            mp1[s[i]]++;
         }
-        for(int j=0;j<n;j++){
-            if(mp[s[j]]==1){
-                return j;
+        for(int i=0;i<n;i++){
+            mp2[s[i]]=i;
+        }
+        // for(auto )
+        vector<char>v;
+        for(auto x:mp1){
+            if(x.second==1){
+               v.push_back(x.first);
             }
         }
-        return -1;
+        if(v.size()==0){
+            return -1;
+        }
+        int mn=INT_MAX;
+        for(int i=0;i<v.size();i++){
+           mn=min(mn,mp2[v[i]]);
+        }
+        return mn;
     }
 };
