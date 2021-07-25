fbefb8651d60c524359abb598703653bc296a20d
Rittik kumar <76815180+Rittik-kumar19@users.noreply.github.com>
2021-07-25 23:51:42 +0530

    Time: 8 ms (95.70%), Space: 10.9 MB (36.62%) - LeetHub

--- two-sum/two-sum.cpp
+++ two-sum/two-sum.cpp
@@ -1,30 +1,30 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        int n=nums.size();
-        vector<int>vnew;
-        for(int i=0;i<n-1;i++){
-            for(int j=i+1;j<n;j++){
-                if(nums[i]+nums[j]==target){
-                    vnew.push_back(i);
-                    vnew.push_back(j);
-                    return vnew;
-                }
-            }
-        }
-        return vnew;
         // int n=nums.size();
         // vector<int>vnew;
-        // unordered_map<int,int>mp;
-        // for(int i=0;i<n;i++){
-        //     if(mp.find(target-nums[i])!=mp.end()){
-        //         vnew.push_back(mp[target-nums[i]]);
-        //         vnew.push_back(i);
-        //         return vnew;
+        // for(int i=0;i<n-1;i++){
+        //     for(int j=i+1;j<n;j++){
+        //         if(nums[i]+nums[j]==target){
+        //             vnew.push_back(i);
+        //             vnew.push_back(j);
+        //             return vnew;
+        //         }
         //     }
-        //     mp[nums[i]]=i;
         // }
         // return vnew;
+        int n=nums.size();
+        vector<int>vnew;
+        unordered_map<int,int>mp;
+        for(int i=0;i<n;i++){
+            if(mp.find(target-nums[i])!=mp.end()){
+                vnew.push_back(mp[target-nums[i]]);
+                vnew.push_back(i);
+                return vnew;
+            }
+            mp[nums[i]]=i;
+        }
+        return vnew;
         // sort(nums.begin(),nums.end());
         // int n=nums.size();
         // vector<int>vnew;
