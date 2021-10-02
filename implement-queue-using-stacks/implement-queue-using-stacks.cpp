4b7d5a103fff9e2968767c9ffbc2ca531010754b
Rittik kumar <76815180+Rittik-kumar19@users.noreply.github.com>
2021-10-02 15:27:26 +0530

    Time: 0 ms (100.00%), Space: 7.1 MB (47.16%) - LeetHub

--- implement-queue-using-stacks/implement-queue-using-stacks.cpp
+++ implement-queue-using-stacks/implement-queue-using-stacks.cpp
@@ -1,53 +1,43 @@
 class MyQueue {
 public:
-    stack<int>s1;
-    stack<int>s2;
-    /** Initialize your data structure here. */
+    stack<int>input,output;
     MyQueue() {
-        
     }
-    
-    /** Push element x to the back of queue. */
     void push(int x) {
-        if(s1.size()>0){
-            while(s1.size()>0){
-                int p=s1.top();
-                s2.push(p);
-                s1.pop();
+        if(input.size()>0){
+            while(input.size()>0){
+                int p=input.top();
+                output.push(p);
+                input.pop();
             }
         }
-        s1.push(x);
-        if(s2.size()>0){
-            while(s2.size()>0){
-                int pp=s2.top();
-                s1.push(pp);
-                s2.pop();
+        input.push(x);
+        if(output.size()>0){
+            while(output.size()>0){
+                int pp=output.top();
+                input.push(pp);
+                output.pop();
             }
         }
     }
-    
-    /** Removes the element from in front of queue and returns that element. */
     int pop() {
-        if(s1.size()>0){
-            int y=s1.top();
-            s1.pop();
+        if(input.size()>0){
+            int y=input.top();
+            input.pop();
             return y;
         }
         return -1;
     }
     
-    /** Get the front element. */
     int peek() {
-        if(s1.size()>0){
-            int z=s1.top();
+        if(input.size()>0){
+            int z=input.top();
             return z;
         }
         return -1;
     }
-    
-    /** Returns whether the queue is empty. */
     bool empty() {
-        if(s1.size()==0){
+        if(input.size()==0){
             return true;
         }
         else {
@@ -55,12 +45,3 @@ public:
         }
     }
 };
-
-/**
- * Your MyQueue object will be instantiated and called as such:
- * MyQueue* obj = new MyQueue();
- * obj->push(x);
- * int param_2 = obj->pop();
- * int param_3 = obj->peek();
- * bool param_4 = obj->empty();
- */
