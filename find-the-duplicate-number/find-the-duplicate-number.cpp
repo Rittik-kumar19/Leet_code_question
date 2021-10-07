fcbcb04a725f793daf5d54266f2095e83b4646ab
Rittik kumar <76815180+Rittik-kumar19@users.noreply.github.com>
2021-10-07 16:03:11 +0530

    Time: 100 ms (80.76%), Space: 61.2 MB (60.07%) - LeetHub

--- find-the-duplicate-number/find-the-duplicate-number.cpp
+++ find-the-duplicate-number/find-the-duplicate-number.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
-        //Tortoise method
+       //Tortoise method
         int slow =nums[0];
         int fast=nums[0];
         do{
