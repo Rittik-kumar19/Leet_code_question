9c437193c31004689e4422cab03575554520e449
Rittik kumar <76815180+Rittik-kumar19@users.noreply.github.com>
2021-12-17 23:37:14 +0530

    Time: 8 ms (17.51%), Space: 11.2 MB (27.77%) - LeetHub

--- search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -1,27 +1,60 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        int n = nums.size();
-        int l = 0,h = n-1;
-        // First find the index of lowest element
-        while(l<h){
-            int m = l + (h-l)/2;
-            if(nums[m]>nums[h]) l = m+1;
-            else h = m; 
+        if(nums.size()==1){
+            if(nums[0]==target) return 0;
+            else return -1;
         }
-        int r = l; // store it in r
+        int pivot = minposi(nums);
+        cout<<pivot<<endl;
+        int lsa=Bs(nums,0,pivot-1,target);
+        int rsa = Bs(nums,pivot,nums.size()-1,target);
+        cout<<lsa<<endl;
+        cout<<rsa<<endl;
+        if(lsa>-1){
+            return lsa;
+        }
+        else{
+            return rsa;
+        }
         
-        if(target==nums[r]) return r;
-        else if(r==0) l=0,h=n-1;
-        else if(target>=nums[0]) l=0,h = r;
-        else if(target<nums[0]) l = r,h=n-1;
-        
-        //Simple Binary Search
-        while(l<=h){
-            int m = l + (h-l)/2;
-            if(nums[m]==target) return m;
-            if(nums[m]>target) h = m -1;
-            else l = m+1;
+    }
+    int Bs(vector<int>& nums, int st, int end,int target){ 
+         while(st<=end){
+            int mid=st+(end-st)/2;
+            if(nums[mid]==target){
+                return mid;
+            }
+            else if(nums[mid]>target){
+                end=mid-1;
+            }
+            else if(nums[mid]<target){
+                st=mid+1;
+            }
+        }
+        // if(i>=0 && nums[i]==target) return i;
+        // if(j>=0 && nums[j]==target) return j;
+        return -1;
+    }
+    int minposi(vector<int>&nums){
+        int n=nums.size();
+        int st=0,end=nums.size()-1;
+        if(n==1){
+            return 0;
+        }
+        while(st<=end){
+            int mid = st + (end-st)/2;
+            int prev = (mid+n-1)%n;
+            int next = (mid+1)%n;
+            if(nums[mid]<nums[prev] and nums[mid]<nums[next]){
+                return mid;
+            }
+            else if(nums[end]<nums[mid]){
+                st=mid+1;
+            }
+            else {
+                end = mid-1;
+            }
         }
         return -1;
     }
