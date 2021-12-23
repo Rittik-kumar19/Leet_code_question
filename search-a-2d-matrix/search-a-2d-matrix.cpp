bd22797a80388866adc592c4771a2460477634aa
Rittik kumar <76815180+Rittik-kumar19@users.noreply.github.com>
2021-12-23 16:50:44 +0530

    Time: 4 ms (74.27%), Space: 9.6 MB (5.60%) - LeetHub

--- search-a-2d-matrix/search-a-2d-matrix.cpp
+++ search-a-2d-matrix/search-a-2d-matrix.cpp
@@ -4,18 +4,31 @@ public:
         
         int m = matrix.size();
         int n = matrix[0].size();
-        int l = 0, r = m*n - 1;
-        
-        while(l<=r){
-            int mid = l + (r-l)/2;
-            if(matrix[mid/n][mid%n]==target){
+        // int l = 0, r = m*n - 1;
+        int i=0,j=n-1;
+        while(i>=0 && i<m && j>=0 && j<n){
+            if(matrix[i][j]==target){
                 return true;
-            }else if(matrix[mid/n][mid%n]>target){
-                r = mid - 1;
-            }else{
-                l = mid + 1;
+            }
+            else if(matrix[i][j]>target){
+                j--;
+            }
+            else if(matrix[i][j]<target){
+                i++;
             }
         }
         return false;
+        
+        // while(l<=r){
+        //     int mid = l + (r-l)/2;
+        //     if(matrix[mid/n][mid%n]==target){
+        //         return true;
+        //     }else if(matrix[mid/n][mid%n]>target){
+        //         r = mid - 1;
+        //     }else{
+        //         l = mid + 1;
+        //     }
+        // }
+        // return false;
     }
 };
