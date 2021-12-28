faf7c280e35a287f64087cf53715e19d33fb8db2
Rittik kumar <76815180+Rittik-kumar19@users.noreply.github.com>
2021-12-29 02:29:34 +0530

    Time: 12 ms (93.52%), Space: 10.3 MB (99.53%) - LeetHub

--- count-negative-numbers-in-a-sorted-matrix/count-negative-numbers-in-a-sorted-matrix.cpp
+++ count-negative-numbers-in-a-sorted-matrix/count-negative-numbers-in-a-sorted-matrix.cpp
@@ -3,7 +3,9 @@ public:
     int countNegatives(vector<vector<int>>& grid) {
         int ans=0;
         for(int i=0;i<grid.size();i++){
+            //use upperbound function of binary search
             int l1=upper_bound(grid[i].rbegin(),grid[i].rend(),-1)-grid[i].rbegin();
+            cout<<l1<<endl;
             ans+=l1;
         }
         return ans;
