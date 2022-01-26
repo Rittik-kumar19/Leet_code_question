97f8e972791a482079270fe2ff0bd97660e23d64
Rittik kumar <76815180+Rittik-kumar19@users.noreply.github.com>
2022-01-27 02:21:01 +0530

    Time: 551 ms (24.15%), Space: 189.6 MB (13.85%) - LeetHub

--- 239-sliding-window-maximum/239-sliding-window-maximum.cpp
+++ 239-sliding-window-maximum/239-sliding-window-maximum.cpp
@@ -27,3 +27,31 @@ public:
         return ans;
     }
 };
+
+
+
+
+// class Solution {
+// public:
+//     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+//        vector<int>vnew;
+//         int n=nums.size();
+//         int i=0,j=0;
+//         while(j<n){
+//             if(j-i+1<k){
+//                 j++;
+//             }
+//             else if(j-i+1==k){
+//                 // int k=i;
+//                 int mx=INT_MIN;
+//                 for(int k=i;k<=j;k++){
+//                    mx=max(mx,nums[k]);
+//                 }
+//                 vnew.push_back(mx);
+//                 j++;
+//                 i++;
+//             }
+//         }
+//         return vnew;
+//     }
+// };
