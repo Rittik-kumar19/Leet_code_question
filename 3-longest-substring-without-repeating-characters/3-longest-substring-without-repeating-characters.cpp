22756a06d80988018ef6ce0617ced74c818d91fe
Rittik kumar <76815180+Rittik-kumar19@users.noreply.github.com>
2022-01-27 02:28:09 +0530

    Time: 12 ms (76.04%), Space: 9 MB (33.68%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
@@ -1,26 +1,56 @@
+// class Solution {
+// public:
+//     int lengthOfLongestSubstring(string s) {
+//         int length = 0;
+//         int max_length = 0;
+//         map<char,int> m;
+//         int i = 0;
+//         for(int j = 0;j<s.size();j++)
+//         {
+//             m[s[j]]++;
+//             if(m[s[j]] > 1)
+//             {
+//                 max_length = max(max_length,length);
+//                 while(m[s[j]] != 1)
+//                 {
+//                     m[s[i]]--;
+//                     length--;
+//                     i++;
+//                 }
+//             }
+//             length++;
+//         }
+//         max_length = max(max_length,length);
+//         return max_length;
+//     }
+// };
+
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        int length = 0;
-        int max_length = 0;
-        map<char,int> m;
+        unordered_map<char,int>mp;
         int i = 0;
-        for(int j = 0;j<s.size();j++)
-        {
-            m[s[j]]++;
-            if(m[s[j]] > 1)
-            {
-                max_length = max(max_length,length);
-                while(m[s[j]] != 1)
-                {
-                    m[s[i]]--;
-                    length--;
+        int j = 0;
+        int mx = INT_MIN;
+        if(s.size()==0){
+            return 0;
+        }
+        while(j<s.size()){
+            mp[s[j]]++;
+            if(mp.size()==j-i+1){
+                mx = max(mx,j-i+1);
+            }
+            else if(mp.size()<j-i+1){
+                while(mp.size()<j-i+1){
+                    mp[s[i]]--;
+                    if(mp[s[i]]==0){
+                        mp.erase(s[i]);
+                    }
                     i++;
                 }
             }
-            length++;
+            j++;
         }
-        max_length = max(max_length,length);
-        return max_length;
+        return mx;
     }
 };
