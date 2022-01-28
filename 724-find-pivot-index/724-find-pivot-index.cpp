159cf4e3a8eab07d80e13658b04ea32e89e7464f
Rittik kumar <76815180+Rittik-kumar19@users.noreply.github.com>
2022-01-28 13:41:29 +0530

    Time: 66 ms (5.79%), Space: 31.7 MB (14.58%) - LeetHub

--- 724-find-pivot-index/724-find-pivot-index.cpp
+++ 724-find-pivot-index/724-find-pivot-index.cpp
@@ -1,17 +1,32 @@
 class Solution {
 public:
     int pivotIndex(vector<int>& nums) {
-        int sum = accumulate(nums.begin(),nums.end(),0);
+        // int sum = accumulate(nums.begin(),nums.end(),0);
+        // int n = nums.size();
+        // int x = sum;
+        // int p = 0;
+        // for(int i=0;i<n;i++){
+        //     if(i>0){
+        //         p=p+nums[i-1];
+        //     }
+        //     x=x-nums[i];
+        //     if(x==p){
+        //         return i;
+        //     }
+        // }
+        // return -1;
+        
         int n = nums.size();
-        int x = sum;
-        int p = 0;
-        for(int i=0;i<n;i++){
-            if(i>0){
-                p=p+nums[i-1];
-            }
-            x=x-nums[i];
-            if(x==p){
-                return i;
+        int sum = accumulate(nums.begin(),nums.end(),0);
+        vector<int>v(n+1);
+        for(int i=1;i<=n;i++){
+            v[i]=v[i-1]+nums[i-1];
+        }
+        for(int i=1;i<=n;i++){
+            int lf = v[i-1];
+            int rs = sum-v[i];
+            if(lf==rs){
+                return (i-1);
             }
         }
         return -1;
