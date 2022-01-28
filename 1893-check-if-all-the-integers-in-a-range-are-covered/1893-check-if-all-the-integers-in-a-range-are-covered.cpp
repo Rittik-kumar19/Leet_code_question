44629d93dfef1dc06ab42ced4d181165ac5e15e0
Rittik kumar <76815180+Rittik-kumar19@users.noreply.github.com>
2022-01-28 11:46:14 +0530

    Time: 23 ms (5.56%), Space: 9.5 MB (5.19%) - LeetHub

--- 1893-check-if-all-the-integers-in-a-range-are-covered/1893-check-if-all-the-integers-in-a-range-are-covered.cpp
+++ 1893-check-if-all-the-integers-in-a-range-are-covered/1893-check-if-all-the-integers-in-a-range-are-covered.cpp
@@ -1,23 +1,43 @@
+// class Solution {
+// public:
+//     bool isCovered(vector<vector<int>>& ranges, int left, int right) {
+//         map<int,bool>mp;
+//         for(int i=left;i<=right;i++){
+//             mp[i]=false;
+//         }
+        
+//         for(int i=0;i<ranges.size();i++){
+//             for(int j=ranges[i][0];j<=ranges[i][1];j++){
+//                 if(mp.find(j)!=mp.end()){
+//                     mp[j]=true;
+//                 }
+//             }
+//         }
+//         for(auto m:mp){
+//             if(m.second==false){
+//                 return false;
+//             }
+//         }
+//         return true;
+//     }
+// };
+
 class Solution {
 public:
     bool isCovered(vector<vector<int>>& ranges, int left, int right) {
-        map<int,bool>mp;
-        for(int i=left;i<=right;i++){
-            mp[i]=false;
-        }
-        
-        for(int i=0;i<ranges.size();i++){
-            for(int j=ranges[i][0];j<=ranges[i][1];j++){
-                if(mp.find(j)!=mp.end()){
-                    mp[j]=true;
-                }
+        set<int>s;
+        for(int j=0;j<ranges.size();j++){
+            vector<int>vnew=ranges[j];
+            for(int i=ranges[j][0];i<=ranges[j][1];i++){
+                s.insert(i);
             }
         }
-        for(auto m:mp){
-            if(m.second==false){
+        for(int k=left;k<=right;k++){
+            if(s.count(k)==0){
                 return false;
             }
         }
         return true;
+        
     }
 };
