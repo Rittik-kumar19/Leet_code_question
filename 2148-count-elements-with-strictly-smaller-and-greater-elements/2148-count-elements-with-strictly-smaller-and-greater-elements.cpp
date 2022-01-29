a486f67a24e6f6a62d74f5b582af641cef728bcb
Rittik kumar <76815180+Rittik-kumar19@users.noreply.github.com>
2022-01-30 01:04:20 +0530

    Time: 5 ms (56.10%), Space: 9.1 MB (43.95%) - LeetHub

--- 2148-count-elements-with-strictly-smaller-and-greater-elements/2148-count-elements-with-strictly-smaller-and-greater-elements.cpp
+++ 2148-count-elements-with-strictly-smaller-and-greater-elements/2148-count-elements-with-strictly-smaller-and-greater-elements.cpp
@@ -1,27 +1,45 @@
+// class Solution {
+// public:
+//     int countElements(vector<int>& nums) {
+//         int n = nums.size();
+//         if(n<=2){
+//             return 0;
+//         }
+//         sort(nums.begin(),nums.end());
+//         int x = 0;
+//         int i =0;
+//         while((i<n-1) and (nums[i]==nums[i+1])){
+//             i++;
+//         }
+//         if(i==n-1){
+//             return 0;
+//         }
+//         x=i+1;
+//         int j =n-1;
+//         int y = 0;
+//         while((j>0)and (nums[j]==nums[j-1])){
+//             j--;
+//         }
+//         y=j-1;
+//         int ans  =  (y-x)+1;
+//         return ans;
+//     }
+// };
+
+
 class Solution {
 public:
     int countElements(vector<int>& nums) {
-        int n = nums.size();
-        if(n<=2){
-            return 0;
+      int n = nums.size();
+      sort(nums.begin(),nums.end());
+      int mini = nums[0];
+      int maxi = nums[n-1];
+        int count = 0;
+        for(int i=0;i<nums.size();i++){
+            if((nums[i]>mini)and(nums[i]<maxi)){
+                count++;
+            }
         }
-        sort(nums.begin(),nums.end());
-        int x = 0;
-        int i =0;
-        while((i<n-1) and (nums[i]==nums[i+1])){
-            i++;
-        }
-        if(i==n-1){
-            return 0;
-        }
-        x=i+1;
-        int j =n-1;
-        int y = 0;
-        while((j>0)and (nums[j]==nums[j-1])){
-            j--;
-        }
-        y=j-1;
-        int ans  =  (y-x)+1;
-        return ans;
+        return  count;
     }
 };
