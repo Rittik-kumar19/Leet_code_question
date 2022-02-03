e769f13729fa05090bf2b522958fed48a8a895b5
Rittik kumar <76815180+Rittik-kumar19@users.noreply.github.com>
2022-02-03 20:40:52 +0530

    Time: 808 ms (25.64%), Space: 100.4 MB (21.43%) - LeetHub

--- 421-maximum-xor-of-two-numbers-in-an-array/421-maximum-xor-of-two-numbers-in-an-array.cpp
+++ 421-maximum-xor-of-two-numbers-in-an-array/421-maximum-xor-of-two-numbers-in-an-array.cpp
@@ -7,15 +7,48 @@ public:
         set<int>se;
         for(int bit = 30;bit>=0;--bit){
             mask |= (1<<bit);
+            //11001
             for(int i=0;i<nums.size();i++){
                 //Jo uske 2 k power mein aa rha wahi le rhe hai
                 se.insert(nums[i]&mask);
+                //10000
+               // 11001
+            //    10000
             }
             int newMax = mx|(1<<bit);
+            // 10000
+            
+            // 10000
+            // 01000
+            // 11000
+            
+            // 11000
+            // 00100
+            // 11100
+            
+//             11100
+//             00010
+//             11110
             
             for(auto prefix:se){
+                //10000
+            //    10000
+                  // 00000
+                
+                // 1000
+                // 1000
+                // 1000
+                
+                // 100
+                // 100
+                // 000
+                    
                 if(se.count(newMax^prefix)){
                     mx = newMax;
+                    
+                    // mx = 16
+                    // mx =24
+                    // mx=28
                     break;
                 }
             }
