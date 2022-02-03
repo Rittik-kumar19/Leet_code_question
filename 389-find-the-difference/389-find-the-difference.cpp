f32ffb33a29fcfc618aa9a52e6ca96d91d73a958
Rittik kumar <76815180+Rittik-kumar19@users.noreply.github.com>
2022-02-03 22:47:00 +0530

    Time: 4 ms (68.21%), Space: 6.9 MB (9.08%) - LeetHub

--- 389-find-the-difference/389-find-the-difference.cpp
+++ 389-find-the-difference/389-find-the-difference.cpp
@@ -1,15 +1,28 @@
 class Solution {
 public:
     char findTheDifference(string s, string t) {
-        int m = t.size();
-        sort(s.begin(),s.end());
-        sort(t.begin(),t.end());
-        int i =0;
-        while(i<m){
-            if(s[i]!=t[i]){
-                return t[i];
+        // int m = t.size();
+        // sort(s.begin(),s.end());
+        // sort(t.begin(),t.end());
+        // int i =0;
+        // while(i<m){
+        //     if(s[i]!=t[i]){
+        //         return t[i];
+        //     }
+        //     i++;
+        // }
+        // return '-1';
+        map<char,int>mp;
+        for(auto ch1:s){
+            mp[ch1]++;
+        }
+        for(auto ch2:t){
+            mp[ch2]++;
+        }
+        for(auto m:mp){
+            if(m.second%2!=0){
+                return m.first;
             }
-            i++;
         }
         return '-1';
     }
