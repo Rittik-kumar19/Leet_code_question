c45fcf755495755e5a43569ac24638f8a1b3a332
Rittik kumar <76815180+Rittik-kumar19@users.noreply.github.com>
2022-04-21 04:20:14 +0530

    Time: 33 ms (68.18%), Space: 24.2 MB (22.24%) - LeetHub

---
+++ 173-binary-search-tree-iterator/173-binary-search-tree-iterator.cpp
@@ -0,0 +1,27 @@
+class BSTIterator {
+public:
+    BSTIterator(TreeNode* root) {
+        inOrder(root);
+    }
+    
+    int next() {
+        curr++;
+        return nodes[curr-1];
+    }
+    
+    bool hasNext() {
+        return curr != nodes.size();
+    }
+    
+private:
+    void inOrder(TreeNode* root) {
+        if (!root)
+            return;
+        inOrder(root->left);
+        nodes.push_back(root->val);
+        inOrder(root->right);
+    }
+    
+    vector<int> nodes;
+    int curr = 0;
+};
