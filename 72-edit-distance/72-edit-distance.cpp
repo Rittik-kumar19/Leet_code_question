baab195c799e64593eb973a3e7b05bba46b4ae09
Rittik kumar <76815180+Rittik-kumar19@users.noreply.github.com>
2022-06-18 03:52:09 +0530

    Time: 28 ms (34.75%), Space: 6.4 MB (94.68%) - LeetHub

--- 72-edit-distance/72-edit-distance.cpp
+++ 72-edit-distance/72-edit-distance.cpp
@@ -15,6 +15,7 @@ public:
             return dp[i][j] = min({1+func(i,j-1,word1,word2,dp),1+func(i-1,j,word1,word2,dp),1+func(i-1,j-1,word1,word2,dp)});
         }
     }
+    /*
     int minDistance(string word1, string word2) {
        int n = word1.size();
        int m = word2.size();
@@ -38,5 +39,29 @@ public:
            }
        }
         return dp[n][m];
+    }
+    */
+       int minDistance(string word1, string word2) {
+       int n = word1.size();
+       int m = word2.size();
+       vector<int>prev(m+1,0),cur(m+1,0);
+       // int ans = func(n,m,word1,word2,dp);
+       // return ans;
+       for(int j=0;j<=m;j++){
+           prev[j]=j;
+       }
+       for(int i=1;i<=n;i++){
+           cur[0]=i;
+           for(int j=1;j<=m;j++){
+               if(word1[i-1]==word2[j-1]){
+                 cur[j] = prev[j-1];
+        }
+        else{
+               cur[j] = min({1+prev[j-1],1+prev[j],1+cur[j-1]});
+        }
+           }
+           prev = cur;
+       }
+        return prev[m];
     }
 };
