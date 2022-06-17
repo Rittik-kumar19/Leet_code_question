a46cdb75768a6cf0634264c8eeb1ef2666a2f83b
Rittik kumar <76815180+Rittik-kumar19@users.noreply.github.com>
2022-06-18 02:10:20 +0530

    Time: 33 ms (85.28%), Space: 6.4 MB (95.91%) - LeetHub

--- 115-distinct-subsequences/115-distinct-subsequences.cpp
+++ 115-distinct-subsequences/115-distinct-subsequences.cpp
@@ -13,6 +13,7 @@ public:
             return dp[i][j] = func(i-1,j,s,t,dp);
         }
     }
+    /*
     int numDistinct(string s, string t) {
         int n = s.size();
         int m = t.size();
@@ -37,6 +38,35 @@ public:
         }
         
         return (int)dp[n][m];
+        
+        
+    }
+    */
+    int numDistinct(string s, string t) {
+        int n = s.size();
+        int m = t.size();
+        vector<double>prev(m+1,0),cur(m+1,0);
+        //base case
+        prev[0]=1;
+        cur[0]=1;
+        
+        // for(int j=0;j<=m;j++){
+        //     dp[0][j]=0;
+        // }
+        
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+               if(s[i-1]==t[j-1]){
+                 cur[j] = prev[j-1] + prev[j];
+                }
+              else{
+                 cur[j] = prev[j];
+               }
+            }
+            prev=cur;
+        }
+        
+        return (int)prev[m];
         
         
     }
