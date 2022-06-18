cb9e6a7475e9633a46d5b8df5a83a9b53bdfa5d7
Rittik kumar <76815180+Rittik-kumar19@users.noreply.github.com>
2022-06-18 17:16:08 +0530

    Time: 21 ms (69.01%), Space: 10.3 MB (87.32%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -1,48 +1,20 @@
 class Solution {
 public:
-    int func(int ind,int prev_ind,vector<int>&nums,int n,vector<vector<int>>&dp){
-        if(ind ==n)return 0;
-        if(dp[ind][prev_ind+1]!=-1){
-            return dp[ind][prev_ind+1];
-        }
-        int len = func(ind+1,prev_ind,nums,n,dp);
-        if(prev_ind == -1 || (nums[ind]>nums[prev_ind])){
-            len = max(len,1+func(ind+1,ind,nums,n,dp));
-        }
-        return dp[ind][prev_ind+1]=len;
-    }
-    // int lengthOfLIS(vector<int>& nums) {
-    //     int n = nums.size();
-    //     vector<vector<int>>dp(n+1,vector<int>(n+1,0));
-    //     // int ans = func(0,-1,nums,n,dp);
-    //     // return ans;
-    //     for(int ind=n-1;ind>=0;ind--){
-    //         for(int prev = ind-1;prev>=-1;prev--){
-    //             int len = dp[ind+1][prev+1];
-    //             if(prev == -1 || (nums[ind]>nums[prev])){
-    //                 len = max(len,1+dp[ind+1][ind+1]);
-    //             }
-    //             dp[ind][prev+1]=len;
-    //         }
-    //     }
-    //     return dp[0][-1+1];
-    // }
     int lengthOfLIS(vector<int>& nums) {
         int n = nums.size();
-        // vector<vector<int>>dp(n+1,vector<int>(n+1,0));
-        vector<int>next(n+1,0),cur(n+1,0);
-        // int ans = func(0,-1,nums,n,dp);
-        // return ans;
-        for(int ind=n-1;ind>=0;ind--){
-            for(int prev = ind-1;prev>=-1;prev--){
-                int len = next[prev+1];
-                if(prev == -1 || (nums[ind]>nums[prev])){
-                    len = max(len,1+next[ind+1]);
-                }
-                cur[prev+1]=len;
+        vector<int>temp;
+        temp.push_back(nums[0]);
+        int len = 1;
+        for(int i=1;i<n;i++){
+            if(nums[i]>temp.back()){
+                temp.push_back(nums[i]);
+                len++;
             }
-            next=cur;
+            else{
+                int ind = lower_bound(temp.begin(),temp.end(),nums[i])-temp.begin();
+                temp[ind]=nums[i];
+            }
         }
-        return next[-1+1];
+        return len;
     }
 };
