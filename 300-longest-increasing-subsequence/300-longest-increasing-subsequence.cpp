eb06388cb6801f6f292b84c07dfd2be21ed8fd78
Rittik kumar <76815180+Rittik-kumar19@users.noreply.github.com>
2022-06-18 16:06:51 +0530

    Time: 675 ms (13.49%), Space: 10.7 MB (21.55%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -11,20 +11,38 @@ public:
         }
         return dp[ind][prev_ind+1]=len;
     }
+    // int lengthOfLIS(vector<int>& nums) {
+    //     int n = nums.size();
+    //     vector<vector<int>>dp(n+1,vector<int>(n+1,0));
+    //     // int ans = func(0,-1,nums,n,dp);
+    //     // return ans;
+    //     for(int ind=n-1;ind>=0;ind--){
+    //         for(int prev = ind-1;prev>=-1;prev--){
+    //             int len = dp[ind+1][prev+1];
+    //             if(prev == -1 || (nums[ind]>nums[prev])){
+    //                 len = max(len,1+dp[ind+1][ind+1]);
+    //             }
+    //             dp[ind][prev+1]=len;
+    //         }
+    //     }
+    //     return dp[0][-1+1];
+    // }
     int lengthOfLIS(vector<int>& nums) {
         int n = nums.size();
-        vector<vector<int>>dp(n+1,vector<int>(n+1,0));
+        // vector<vector<int>>dp(n+1,vector<int>(n+1,0));
+        vector<int>next(n+1,0),cur(n+1,0);
         // int ans = func(0,-1,nums,n,dp);
         // return ans;
         for(int ind=n-1;ind>=0;ind--){
             for(int prev = ind-1;prev>=-1;prev--){
-                int len = dp[ind+1][prev+1];
+                int len = next[prev+1];
                 if(prev == -1 || (nums[ind]>nums[prev])){
-                    len = max(len,1+dp[ind+1][ind+1]);
+                    len = max(len,1+next[ind+1]);
                 }
-                dp[ind][prev+1]=len;
+                cur[prev+1]=len;
             }
+            next=cur;
         }
-        return dp[0][-1+1];
+        return next[-1+1];
     }
 };
